a5b75b387bb9c8ab94a53b46335b5cec2312aac8
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-07-16 12:28:15 -0400

    add hero icons

---
+++ admin-template/src/constants/index.js
@@ -0,0 +1,25 @@
+export const navLinks = [
+  {
+    id: "dashboard",
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: "home",
+    exact: true,
+  },
+  {
+    id: "users",
+    title: "Users",
+    path: "/users",
+    icon: "user",
+    exact: true,
+  },
+  {
+    id: "products",
+    title: "Products",
+    path: "/products",
+    icon: "shopping-cart",
+    exact: true,
+  },
+];
+
+export { navLinks };
